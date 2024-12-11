ea73fa00ca36b5f784025b18a224bc6c243263ba
Subhangee Sahoo <shubh190601@gmail.com>
2024-12-11 22:42:09 +0530

    Added

---
+++ backend/src/middleware/auth.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+declare global {
+  namespace Express {
+    interface Request {
+      userId: string;
+    }
+  }
+}
+
+const verifyToken = (req: Request, res: Response, next: NextFunction) => {
+  //console.log('Verify token....................',req);
+  const token = req.cookies["auth_token"] || process.env.DEFAULT_TOKEN;
+  try {
+    //console.log('token',token);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY as string);
+    req.userId = (decoded as JwtPayload).userId;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "unauthorized" });
+  }
+};
+
+export default verifyToken;
